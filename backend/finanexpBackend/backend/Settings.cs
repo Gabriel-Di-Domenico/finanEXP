c9593ff66dcd6277b31b9d2b0d83b98abcb89652
Azfan0078 <gabrielddomenico@gmail.com>
2022-09-24 13:07:06 -0300

    fix .gitignore

--- backend/finanexpBackend/backend/Settings.cs
+++
@@ -1,8 +0,0 @@
-namespace backend
-{
-  public static class Settings
-  {
-    public static string Secret = "Y,3{llbi>,Pqff97j,D6i;jc|yd@WZeq";
-    public static string DatabasePassword = "Nhonhoc33";
-  }
-}
