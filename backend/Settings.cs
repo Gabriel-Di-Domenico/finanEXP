153b3fcb226edd1e9e9fc9f6d59c9545a2c0d07b
Azfan0078 <gabrielddomenico@gmail.com>
2022-10-02 16:52:10 -0300

    deleted:    backend/Settings.cs

--- backend/Settings.cs
+++
@@ -1,9 +0,0 @@
-namespace backend
-{
-  public class Settings
-  {
-    public static string Secret = "sASD!@A564AaSDfaçl!@%$#132asd!5asd34!@3";
-    public static string DatabasePassword = "Nhonhoc3";
-    public static string DataBaseName = "finanEXP-development";
-  }
-}
