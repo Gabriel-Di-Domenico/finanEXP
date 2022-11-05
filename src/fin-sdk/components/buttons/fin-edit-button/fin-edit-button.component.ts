d35ef38b6edd5932edd597726e2eae0ac43a5e83
Azfan0078 <gabrielddomenico@gmail.com>
2022-11-05 14:13:01 -0300

    Botoes agora mais dinamicos

--- src/fin-sdk/components/buttons/fin-edit-button/fin-edit-button.component.ts
+++ src/fin-sdk/components/buttons/fin-edit-button/fin-edit-button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'fin-edit-button',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./fin-edit-button.component.css']
 })
 export class FinEditButtonComponent {
-
+  @Input() color = 'primary';
 }
