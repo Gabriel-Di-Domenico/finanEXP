137eda2d24459cb439f117798f0fd2d18c29f18d
Azfan0078 <gabrielddomenico@gmail.com>
2023-02-02 19:37:14 -0300

    Criado testes da controller de category

---
+++ backend/finan-exp-backend-tests/Categories/Services/CategoriesServiceTests.cs
@@ -0,0 +1,18 @@
+using Categories.Services;
+using Contexts;
+
+namespace finan_exp_backend_tests.Categories.Services
+{
+  public class CategoriesServiceTests
+  {
+    [Fact]
+    public void CreateCategory()
+    {
+      //Arrange
+     
+      //Act
+
+      //Assert
+    }
+  }
+}
