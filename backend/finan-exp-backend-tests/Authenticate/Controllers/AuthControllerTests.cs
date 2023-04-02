a1851c1c4d2964f21c4537bbf230eaee7b6d4d72
Azfan0078 <gabrielddomenico@gmail.com>
2023-04-02 10:50:40 -0300

    Corrigido teste

--- backend/finan-exp-backend-tests/Authenticate/Controllers/AuthControllerTests.cs
+++ backend/finan-exp-backend-tests/Authenticate/Controllers/AuthControllerTests.cs
@@ -6,6 +6,9 @@ using finan_exp_backend_tests.Supports;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using NSubstitute;
+using Shared.Dtos;
+using Shared.Enums;
+using Shared.Messages;
 using Users.Services;
 
 namespace finan_exp_backend_tests.Authenticate.Controllers
@@ -13,29 +16,121 @@ namespace finan_exp_backend_tests.Authenticate.Controllers
   public class AuthControllerTests
   {
     [Fact(DisplayName = "Auth user with successfully")]
-    public void AuthenticateUserWithSuccessfully()
+    public async Task AuthenticateUserWithSuccessfully()
     {
       //Arrange
       var mockUserDataBaseService = Substitute.For<IUserDatabaseService>();
     
-      var mockAuthUserService = Substitute.For<AuthUserService>();
-
-      var authController = new AuthController(mockUserDataBaseService, mockAuthUserService);
-
-      var mockUser = new UserAuthDto
+      var mockAuthUserService = Substitute.For<IAuthUserService>();
+      var mockAuthUser = new UserAuthDto
       {
         Email = TestUtils.MockValidEmails[0],
         Name = TestUtils.MockStrings[0],
         Password = TestUtils.MockStrings[0]
       };
+      mockUserDataBaseService.GetUserByEmail(TestUtils.MockValidEmails[0]).Returns(TestUtils.MockUsers[0]);
+      var responseStatus = new ResponseStatus<string>
+      {
+        Content = TestUtils.MockJwts[0],
+        Status = ResponseStatus.Ok
+      };
+
+      mockAuthUserService.AuthUser(mockAuthUser, TestUtils.MockUsers[0]).Returns(responseStatus);
+
+      var authController = new AuthController(mockUserDataBaseService, mockAuthUserService);
+
+      
       //Act
-      var result = authController.AuthenticateAsync(mockUser);
+      var result = await authController.AuthenticateAsync(mockAuthUser);
 
       //Assert
-      var output = result.Result as OkResult;
-      
+      var output = result.Result as OkObjectResult;
+      var value = output.Value as ReturnDto<string>;
+
       output.StatusCode.Should().Be(200);
-     
+      value.Message.Should().BeEquivalentTo(new Message {
+        error = false,
+        message = "Usuário autenticado"
+      });
+      value.Content.Should().Be(TestUtils.MockJwts[0]);
+
+    }
+
+    [Fact(DisplayName = "Auth user unauthorized")]
+    public async Task AuthenticateUserUnauthorized()
+    {
+      //Arrange
+      var mockUserDataBaseService = Substitute.For<IUserDatabaseService>();
+
+      var mockAuthUserService = Substitute.For<IAuthUserService>();
+      var mockAuthUser = new UserAuthDto
+      {
+        Email = TestUtils.MockValidEmails[0],
+        Name = TestUtils.MockStrings[0],
+        Password = TestUtils.MockStrings[0]
+      };
+      mockUserDataBaseService.GetUserByEmail(TestUtils.MockValidEmails[0]).Returns(TestUtils.MockUsers[0]);
+      var responseStatus = new ResponseStatus<string>
+      {
+        Content = null,
+        Status = ResponseStatus.Unauthorized
+      };
+
+      mockAuthUserService.AuthUser(mockAuthUser, TestUtils.MockUsers[0]).Returns(responseStatus);
+
+      var authController = new AuthController(mockUserDataBaseService, mockAuthUserService);
+
+
+      //Act
+      var result = await authController.AuthenticateAsync(mockAuthUser);
+
+      //Assert
+      var output = result.Result as UnauthorizedObjectResult;
+
+      var value = output.Value as ReturnDto<string>;
+
+      output.StatusCode.Should().Be(401);
+      value.Message.Should().BeEquivalentTo(new Message
+      {
+        error = true,
+        message = "Usuário não autorizado"
+      });
+      value.Content.Should().BeNull();
+
+    }
+    [Fact(DisplayName = "Auth user not registered")]
+    public async Task AuthenticateUserNotRegistered()
+    {
+      //Arrange
+      var mockUserDataBaseService = Substitute.For<IUserDatabaseService>();
+
+      var mockAuthUserService = Substitute.For<IAuthUserService>();
+      var mockAuthUser = new UserAuthDto
+      {
+        Email = TestUtils.MockValidEmails[0],
+        Name = TestUtils.MockStrings[0],
+        Password = TestUtils.MockStrings[0]
+      };
+
+      var authController = new AuthController(mockUserDataBaseService, mockAuthUserService);
+
+
+      //Act
+      var result = await authController.AuthenticateAsync(mockAuthUser);
+
+      //Assert
+      var output = result.Result as UnauthorizedObjectResult;
+
+      var value = output.Value as ReturnDto<string>;
+
+      output.StatusCode.Should().Be(401);
+      value.Message.Should().BeEquivalentTo(new Message
+      {
+        error = true,
+        message = "Usuário não Registrado"
+      });
+      value.Content.Should().BeNull();
+
     }
   }
 }
